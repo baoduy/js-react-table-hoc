a354f6d2b2ef0abbe0c68c2749557c8717569ce7
Steven <hoangbaoduy@gmail.com>
2018-07-26 22:04:08 +0800

    update comment
    update comment

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-//Review here for full code of this samples.
+//Refer here https://codesandbox.io/s/8pkrj5yorl for full code of this samples.
 ReactDOM.render(<App />, document.getElementById("root"));
